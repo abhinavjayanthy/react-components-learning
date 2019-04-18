0303420827ff5c764aaa288d2473ab94bd709970
Abhinav <abhinavjayanthy@icloud.com>
2019-04-18 12:37:02 -0400

    Instaling faker to use fake image

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () => (
   <div className="ui container comments">
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatr" />
+        <img alt="avatr" src={faker.image.avatar()} />
       </a>
       <div className="content">
         <a href="/" className="author">
