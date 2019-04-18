6dfb3611b3ad9715de1c0b32b0bc373d6fcdc22f
Abhinav <abhinavjayanthy@icloud.com>
2019-04-18 12:39:18 -0400

    copy paste blunder

--- src/index.js
+++ src/index.js
@@ -19,6 +19,34 @@ const App = () => (
         <div className="text">Nice Blog Post</div>
       </div>
     </div>
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatr" src={faker.image.avatar()} />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          Bunny
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 6:30pm</span>
+        </div>
+        <div className="text">Nice Blog Post 1</div>
+      </div>
+    </div>
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatr" src={faker.image.avatar()} />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          Chill
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 6:45pm</span>
+        </div>
+        <div className="text">Nice Blog Post 3</div>
+      </div>
+    </div>
   </div>
 );
 ReactDOM.render(<App />, document.querySelector('#root'));
