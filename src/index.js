d1ae7299f60a4c13643df5cd88f44effd13dfdaa
Abhinav <abhinavjayanthy@icloud.com>
2019-04-18 12:30:17 -0400

    Basic comment

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <div> Hi There!</div>;
+const App = () => (
+  <div className="ui container comments">
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatr" />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          Sam
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 6:00pm</span>
+        </div>
+        <div className="text">Nice Blog Post</div>
+      </div>
+    </div>
+  </div>
+);
 ReactDOM.render(<App />, document.querySelector('#root'));
